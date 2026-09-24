c04fb0942c76c889a73eb71f817d9559efcda1e3
Jose Manuel Rosa Moncayo <josemanuel.rosamoncayo+github@gmail.com>
2026-09-24 13:44:39 +0200

    chore: update version

--- kura/__version__.py
+++ kura/__version__.py
@@ -4,4 +4,4 @@ Bump this to match the tag (without the leading `v`) before pushing a release.
 Development checkouts may carry a `-dev` suffix between tags.
 """
 
-__version__ = "0.6.0-dev"
+__version__ = "0.6.1"
